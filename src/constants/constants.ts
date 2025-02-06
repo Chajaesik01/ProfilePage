564564ac4e3628d7f6f494093b5caf7811a76337
chajaesik01 <ckwotlr@dongguk.edu>
2025-02-06 17:46:06 +0900

    chore: 절대경로 alias 설정

---
+++ src/constants/constants.ts
@@ -0,0 +1,11 @@
+const ROUTER_PATH = {
+  HOME: '/home',
+  STUDY: 'study',
+};
+
+const NAVIGATION_BAR = {
+  HOME: 'HOME',
+  STUDY: 'STUDY',
+};
+
+export { ROUTER_PATH, NAVIGATION_BAR };
---
+++ src/routes/Router.tsx
@@ -0,0 +1,7 @@
+//import { ROUTER_PATH } from '@/constants/constants'
+
+const Router = () => {
+  return <div>Router</div>;
+};
+
+export default Router;
